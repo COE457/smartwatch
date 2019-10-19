3b54ac5715769228e91b52968bb1637fe839f793
lanahajhussain <lanahajhussain@yahoo.com>
2019-10-19 19:14:03 +0400

    Added the back key implementation

--- TAUBasic/js/app.js
+++ TAUBasic/js/app.js
@@ -48,6 +48,28 @@ var idcount=0;
 
 }());
 
+
+
+(function() {
+	window.addEventListener("tizenhwkey", function(ev) {
+		var activePopup = null, page = null, pageId = "";
+
+		if (ev.keyName === "back") {
+			activePopup = document.querySelector(".ui-popup-active");
+			page = document.getElementsByClassName("ui-page-active")[0];
+			pageId = page ? page.id : "";
+
+			if (pageId === "hsectionchangerPage" && !activePopup) {
+				try {
+					tizen.application.getCurrentApplication().exit();
+				} catch (ignore) {
+				}
+			} else {
+				window.history.back();
+			}
+		}
+	});
+}());
 /**
  * @function remove
  * @desciption generates the message to be previewed
